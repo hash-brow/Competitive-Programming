e1ddb8e97d620a4fba0bd8c19f8ad45cdaeedce8
windreaper <div4804@gmail.com>
2021-07-15 12:16:59 +0530

    full solve for banks

--- POI/banks.cpp
+++ POI/banks.cpp
@@ -1,63 +1,97 @@
 #include <bits/stdc++.h>
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+*/
 
 using namespace std;
+// using namespace __gnu_pbds;
+using namespace chrono;
 
-vector<bool> found;
-vector<int> l[1000000];
-int col[1000000];
+// mt19937 rng((int) std::chrono::steady_clock::now().time_since_epoch().count());
+/*
+template <class T> using ordered_set = tree <T, null_type, less <T>, rb_tree_tag, tree_order_statistics_node_update>;
+*/
 
-int cycle_start;
+//***************** CONSTANTS *****************
 
-bool dfs(int node){
-	col[node] = 1;
 
-	for(int v : l[node]){
-		if(col[v] == 1){
-			cycle_start = v;
-			return true;
-		}
-		if(dfs(v))
-			return true;
+
+//***************** GLOBAL VARIABLES *****************
+
+const int MXN = 1000000;
+
+//***************** AUXILIARY STRUCTS *****************
+
+struct UFDS{
+	int e[MXN], cnt;
+	UFDS(int N){
+		memset(e, -1, sizeof e);
+		cnt = N;
 	}
-	col[node] = 2;
 
-	return false;
-}
+	int get(int x) { return (e[x] < 0 ? x : e[x] = get(e[x])); }
 
-void mark(int node){
-	if(found[node])
-		return;
-	found[node] = 1;
-	for(int v : l[node])
-		mark(v);
-}
+	void unite(int x, int y){
+		x = get(x), y = get(y);
+		if(x == y)
+			return;
 
-int32_t main(){
-	ios_base::sync_with_stdio(false); 
-	cin.tie(NULL); 
+		cnt--;
+		if(e[x] < e[y])
+			swap(x, y);
 
-	int n;
-	cin >> n;
+		e[x] += e[y];
+		e[y] = x;
+	}
+};
 
-	found.resize(n);
+//***************** MAIN BODY *****************
 
-	for(int i = 0; i < n; i++){
+void solve(){
+	int N;
+	cin >> N;
+
+	UFDS D(N);
+
+	for(int i = 0; i < N; i++){
 		int x;
 		cin >> x;
-		--x;
-		l[x].emplace_back(i);
+		D.unite(i, x - 1);
 	}
 
-	int ans = 0;
-
-	for(int i = 0; i < n; i++){
-		cycle_start = -1;
-		if(!found[i] && col[i] == 0 && dfs(i)){
-			mark(cycle_start);
-			ans++;
-		}
-	}
-
-	cout << ans << '\n';
-	return 0; 
+	cout << D.cnt << '\n';
 }
+
+//***************** *****************
+
+int32_t main(){
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+
+	#ifdef LOCAL
+		auto begin = high_resolution_clock::now();
+	#endif
+
+	int tc = 1;
+	// cin >> tc; 
+	for (int t = 0; t < tc; t++)
+		solve();
+
+	#ifdef LOCAL 
+		auto end = high_resolution_clock::now();
+		cout << fixed << setprecision(4);
+		cout << "Execution Time: " << duration_cast<duration<double>>(end - begin).count() << "seconds" << endl;
+	#endif
+
+	return 0;
+}
+
+/*
+If code gives a WA, check for the following : 
+1. I/O format
+
+2. Are you clearing all global variables in between tests if multitests are a thing
+
+3. Can you definitively prove the logic
+*/
